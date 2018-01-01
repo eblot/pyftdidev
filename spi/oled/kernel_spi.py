a2fbda5518b9bfb324c25e2e5303b5b45200b88c
Emmanuel Blot <emmanuel.blot@free.fr>
2018-01-01 20:13:44 +0100

    Debug session

--- spi/oled/kernel_spi.py
+++ spi/oled/kernel_spi.py
@@ -13,15 +13,13 @@ class Ssd1306KernelPort:
 
     def __init__(self, debug=False):
         self._debug = debug
-        self._spi = SpiDev()
         self._spi_port = None
-        self._io_dc_port = None
-        self._io_reset_port = None
 
     def open(self):
         """Open an SPI connection to a slave"""
-        self._spi_port = self._spi.open(0, 0)
-        self._spi_port.max_speed_hz = int(3E6)
+        self._spi_port = SpiDev()
+        self._spi_port.open(0, 0)
+        self._spi_port.max_speed_hz = 5000
         self._spi_port.mode = 0b00
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
@@ -33,20 +31,19 @@ class Ssd1306KernelPort:
         self._spi_port.close()
 
     def reset(self):
-        self._io = self.RESET_PIN
-        GPIO.output(self._io_reset_port, True)
+        GPIO.output(self.RESET_PIN, True)
         sleep(0.001)
-        GPIO.output(self._io_reset_port, False)
+        GPIO.output(self.RESET_PIN, False)
         sleep(0.001)
-        GPIO.output(self._io_reset_port, True)
+        GPIO.output(self.RESET_PIN, True)
         sleep(0.001)
 
     def write_command(self, data):
-        GPIO.output(self._io_dc_port, False)
+        GPIO.output(self.DC_PIN, False)
         self._spi_port.writebytes(data)
 
     def write_data(self, data):
-        GPIO.output(self._io_dc_port, True)
+        GPIO.output(self.DC_PIN, True)
         self._spi_port.writebytes(data)
 
 
